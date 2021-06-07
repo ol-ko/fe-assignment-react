3f647f253ef7d437b69a7786bc1a9cf11900807e
Olga Skurativska <olga.skurativska@homegate.ch>
2021-06-07 12:52:10 +0300

    removed classes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 import './App.scss';
 
 import ListingList from './components/ListingList';
 import ListingDetails from './components/ListingDetails';
 
-class App extends Component {
-    render() {
-        return (
-            <Router>
-                <div>
-                    <nav>
-                        <Link to="/">Properties for rent</Link>
-                    </nav>
+function App() {
+    return (
+        <Router>
+            <div>
+                <nav>
+                    <Link to="/">Properties for rent</Link>
+                </nav>
 
-                    <div className="App">
-                        <Route exact path="/" component={ListingList}/>
-                        <Route exact path="/listings/:listingId" component={ListingDetails}/>
-                    </div>
+                <div className="App">
+                    <Route exact path="/" component={ListingList}/>
+                    <Route exact path="/listings/:listingId" component={ListingDetails}/>
                 </div>
-            </Router>
-        );
-    }
+            </div>
+        </Router>
+    );
 }
 
 export default App;
--- src/components/ListingDetails.js
+++ src/components/ListingDetails.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class ListingDetails extends Component {
-    render() {
-        return (
-            <div>
-                <section>
-                    <h1>Task 2: Listing details go here:</h1>
-                    <p>TODO: display listing picture, title and description</p>
-                </section>
+function ListingDetails() {
+    return (
+        <div>
+            <section>
+                <h1>Task 2: Listing details go here:</h1>
+                <p>TODO: display listing picture, title and description</p>
+            </section>
 
-                <section>
-                    <h1>Task 3: Similar listings go here:</h1>
-                    <p>TODO: Display a list of the similar listings (just picture and title for each of them)</p>
-                </section>
-            </div>
-        )
-    }
+            <section>
+                <h1>Task 3: Similar listings go here:</h1>
+                <p>TODO: Display a list of the similar listings (just picture and title for each of them)</p>
+            </section>
+        </div>
+    )
 }
 
 export default ListingDetails;
